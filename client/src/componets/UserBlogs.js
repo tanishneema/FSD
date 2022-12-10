0afafdf2ed2848215b295f5df38bde081dc6cdd1
Tanish Neema <tneema49@gmail.com>
2022-12-10 12:11:16 +0530

    Error Solving

--- client/src/componets/UserBlogs.js
+++ client/src/componets/UserBlogs.js
@@ -4,21 +4,21 @@ import Blogs from "./Blogs";
 const UserBlogs = () => {
   const [user, setUser] = useState();
   const id = localStorage.getItem("userId");
-  const sendRequest = async () => {
-    const res = await axios
-      .get(`http://localhost:5000/api/blogs/user/${id}`)
-      .catch((err) => console.log(err));
-    const data = await res.data;
-    console.log(data);
-    console.log("-----");
-    return data;
-  };
   useEffect(() => {
+    const sendRequest = async () => {
+      const res = await axios
+        .get(`http://localhost:5000/api/blogs/user/${id}`)
+        .catch((err) => console.log(err));
+      const data = await res.data;
+      console.log(data);
+      console.log("-----");
+      return data;
+    };
     sendRequest().then((data) => setUser(data.user));
     console.log("-----------+");
     console.log(user);
     console.log("--------------+");
-  }, []);
+  }, [id, user]);
 
   return (
     <div>
